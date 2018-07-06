91efc9f83f733599903de2dede95147aeb7b3d5d
royces9 <rrysato@gmail.com>
2018-07-06 10:15:51 -0700

    delete.h: forgot to add delete.h christ

---
+++ delete.h
@@ -0,0 +1,14 @@
+#ifndef DELETE
+#define DELETE
+
+enum errorEnum {
+  deleteFileError = -1,
+  removeFileError = -2,
+  directoryExist = -3,
+  fileExist = -4,
+  mallocError = -5,
+  makeDirectoryError = -6,
+  removeDirError = -7
+};
+
+#endif //DELETE
